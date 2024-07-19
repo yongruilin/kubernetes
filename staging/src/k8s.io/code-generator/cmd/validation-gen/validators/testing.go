2233996941e3a6364bee380bddc1746d08e785e4
Tim Hockin <thockin@google.com>
2024-07-19 15:19:50 -0700

    Add a fixed-result validator for testing
    The tags `+validateTrue` and `+validateFalse` will always pass or fail
    validation, respectively.  They can have no value or they can have a
    string value, which will be passed to the validation logic.
    
    This is useful to force various conditions in the validation-gen tool,
    and helps when reading the emitted code.

---
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/validators/testing.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package validators
+
+import (
+	"strings"
+
+	"k8s.io/gengo/v2"
+	"k8s.io/gengo/v2/generator"
+	"k8s.io/gengo/v2/types"
+)
+
+func init() {
+	AddToRegistryPriority(InitValidateTrueDeclarativeValidator)
+	AddToRegistryPriority(InitValidateFalseDeclarativeValidator)
+}
+
+func InitValidateTrueDeclarativeValidator(c *generator.Context) DeclarativeValidator {
+	return &fixedResultDeclarativeValidator{true}
+}
+
+func InitValidateFalseDeclarativeValidator(c *generator.Context) DeclarativeValidator {
+	return &fixedResultDeclarativeValidator{false}
+}
+
+type fixedResultDeclarativeValidator struct {
+	result bool
+}
+
+const (
+	// These tags can take no value or a string, which will be used in the
+	// error message.
+	validateTrueTagName  = "validateTrue"  // TODO: also support k8s:...
+	validateFalseTagName = "validateFalse" // TODO: also support k8s:...
+)
+
+var (
+	fixedResultValidator = types.Name{Package: libValidationPkg, Name: "FixedResult"}
+)
+
+func (v fixedResultDeclarativeValidator) ExtractValidations(field string, t *types.Type, comments []string) ([]FunctionGen, error) {
+	var result []FunctionGen
+
+	if v.result {
+		vals, fixedTrue := gengo.ExtractCommentTags("+", comments)[validateTrueTagName]
+		if fixedTrue {
+			result = append(result, Function(validateTrueTagName, fixedResultValidator, true, strings.Join(vals, ",")))
+		}
+	} else {
+		vals, fixedFalse := gengo.ExtractCommentTags("+", comments)[validateFalseTagName]
+		if fixedFalse {
+			result = append(result, Function(validateFalseTagName, fixedResultValidator, false, strings.Join(vals, ",")))
+		}
+	}
+
+	return result, nil
+}
