6739a67ffce6dd86fb6f624cd76da43b10daa670
Tim Hockin <thockin@google.com>
2024-07-20 14:18:08 -0700

    Add an output test for more primitives

---
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/primitives/doc.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +k8s:validation-gen=TypeMeta
+
+// This is a test package.
+package primitives
+
+type T1 struct {
+	TypeMeta int
+	// +validateTrue="field T1.S"
+	S string `json:"s"`
+	// +validateTrue="field T1.I"
+	I int `json:"i"`
+	// +validateTrue="field T1.B"
+	B bool `json:"b"`
+	// +validateTrue="field T1.F"
+	F float64 `json:"f"`
+
+	// Note: not tag here because of a bug.
+	T2 T2 `json:"t2"`
+}
+
+type T2 struct {
+	// +validateTrue="field T2.S"
+	S string `json:"s"`
+	// +validateTrue="field T2.I"
+	I int `json:"i"`
+	// +validateTrue="field T2.B"
+	B bool `json:"b"`
+	// +validateTrue="field T2.F"
+	F float64 `json:"f"`
+}
