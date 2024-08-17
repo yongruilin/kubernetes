9a06845cae887247ff6521dd5bc0643ea50c70a3
Joe Betz <jpbetz@google.com>
2024-08-16 21:49:14 -0400

    Clean up error reporting for union validation

--- staging/src/k8s.io/apimachinery/pkg/api/validate/union.go
+++ staging/src/k8s.io/apimachinery/pkg/api/validate/union.go
@@ -19,8 +19,6 @@ package validate
 import (
 	"fmt"
 	"reflect"
-	"sort"
-	"strconv"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/util/validation/field"
@@ -33,13 +31,13 @@ import (
 // For example:
 //
 //	var abcUnionMembership := schema.NewUnionMembership("a", "b", "c")
-//	func ValidateABC(fldPath, *field.Path, in *ABC) (errs fields.ErrorList) {
+//	func ValidateABC(fldPath *field.Path, in *ABC) (errs fields.ErrorList) {
 //		errs = append(errs, Union(fldPath, in, abcUnionMembership, in.A, in.B, in.C)...)
 //		return errs
 //	}
 func Union(fldPath *field.Path, in any, union *UnionMembership, fieldValues ...any) field.ErrorList {
 	if len(union.members) != len(fieldValues) {
-		return field.ErrorList{field.InternalError(fldPath, fmt.Errorf("unexpected difference in length between fields defined in UnionMembership as fieldValues"))}
+		return field.ErrorList{field.InternalError(fldPath, fmt.Errorf("unexpected difference in length between fields defined in UnionMembership and fieldValues"))}
 	}
 	var specifiedMember *string
 	for i, fieldValue := range fieldValues {
@@ -47,14 +45,19 @@ func Union(fldPath *field.Path, in any, union *UnionMembership, fieldValues ...a
 		if rv.IsValid() && !rv.IsZero() {
 			m := union.members[i]
 			if specifiedMember != nil && *specifiedMember != m.memberName {
-				return field.ErrorList{field.Invalid(fldPath, "", fmt.Sprintf("must not specify %s, must specify one of: %s", union.specifiedFields(fieldValues), union.allFields()))}
+				return field.ErrorList{
+					field.Invalid(fldPath, fmt.Sprintf("{%s}", strings.Join(union.specifiedFields(fieldValues), ", ")),
+						fmt.Sprintf("must specify exactly one of %s", strings.Join(union.allFields(), ", "))),
+				}
 			}
 			name := m.memberName
 			specifiedMember = &name
 		}
 	}
 	if specifiedMember == nil {
-		return field.ErrorList{field.Invalid(fldPath, "", fmt.Sprintf("must specify one of: %s", union.allFields()))}
+		return field.ErrorList{field.Invalid(fldPath, "",
+			fmt.Sprintf("must specify exactly one of: %s",
+				strings.Join(union.allFields(), ", ")))}
 	}
 	return nil
 }
@@ -81,9 +84,15 @@ func DiscriminatedUnion[T ~string](fldPath *field.Path, in any, union *UnionMemb
 		rv := reflect.ValueOf(fieldValue)
 		isSpecified := rv.IsValid() && !rv.IsZero()
 		if isSpecified && !isDiscriminatedMember {
-			errs = append(errs, field.Invalid(fldPath.Child(member.fieldName), "", fmt.Sprintf("must not be specified when %s is %s", strconv.Quote(fmt.Sprint(union.discriminatorName)), strconv.Quote(fmt.Sprint(discriminatorValue)))))
+			errs = append(errs, field.Invalid(fldPath.Child(member.fieldName), "",
+				fmt.Sprintf("may only be specified when `%s` is %q",
+					fmt.Sprint(union.discriminatorName),
+					fmt.Sprint(discriminatorValue))))
 		} else if !isSpecified && isDiscriminatedMember {
-			errs = append(errs, field.Invalid(fldPath.Child(member.fieldName), "", fmt.Sprintf("must be specified when %s is %s", strconv.Quote(fmt.Sprint(union.discriminatorName)), strconv.Quote(fmt.Sprint(discriminatorValue)))))
+			errs = append(errs, field.Invalid(fldPath.Child(member.fieldName), "",
+				fmt.Sprintf("may only be specified when `%s` is %q",
+					fmt.Sprint(union.discriminatorName),
+					fmt.Sprint(discriminatorValue))))
 		}
 	}
 	return errs
@@ -101,10 +110,8 @@ type UnionMembership struct {
 
 // NewUnionMembership returns a new UnionMembership for the given list of members.
 //
-// Each member entry may one of:
-//   - string: To provide the fieldName of a field union member where the union member name is CamelCase of the fieldName.
-//   - [2]string: To provide a fieldName and memberName pair, where [0] identifies the field fieldName and [1] identifies the
-//     union member Name.
+// Each member is a [2]string to provide a fieldName and memberName pair, where
+// [0] identifies the field name and [1] identifies the union member Name.
 //
 // Field names must be unique.
 func NewUnionMembership(member ...any) *UnionMembership {
@@ -118,8 +125,6 @@ func NewDiscriminatedUnionMembership(discriminatorFieldName string, members ...a
 	u.discriminatorName = discriminatorFieldName
 	for _, fieldName := range members {
 		switch f := fieldName.(type) {
-		case string:
-			u.members = append(u.members, member{fieldName: f, memberName: strings.ToUpper(f[:1]) + f[1:]})
 		case [2]string:
 			u.members = append(u.members, member{fieldName: f[0], memberName: f[1]})
 		}
@@ -128,35 +133,23 @@ func NewDiscriminatedUnionMembership(discriminatorFieldName string, members ...a
 }
 
 // specifiedFields returns a string listing all the field names of the specified fieldValues for use in error reporting.
-func (u UnionMembership) specifiedFields(fieldValues []any) string {
+func (u UnionMembership) specifiedFields(fieldValues []any) []string {
 	var membersSpecified []string
 	for i, fieldValue := range fieldValues {
 		rv := reflect.ValueOf(fieldValue)
 		if rv.IsValid() && !rv.IsZero() {
 			f := u.members[i]
-			membersSpecified = append(membersSpecified, strconv.Quote(fmt.Sprint(f.fieldName)))
+			membersSpecified = append(membersSpecified, f.fieldName)
 		}
 	}
-	return joinWithSeparators(membersSpecified, ", ", "and")
+	return membersSpecified
 }
 
-// specifiedFields returns a string listing all the field names of the member of a union for use in error reporting.
-func (u UnionMembership) allFields() string {
+// allFields returns a string listing all the field names of the member of a union for use in error reporting.
+func (u UnionMembership) allFields() []string {
 	memberNames := make([]string, 0, len(u.members))
 	for _, f := range u.members {
-		memberNames = append(memberNames, strconv.Quote(fmt.Sprint(f.fieldName)))
+		memberNames = append(memberNames, fmt.Sprintf("`%s`", f.fieldName))
 	}
-	return joinWithSeparators(memberNames, ", ", "or")
-}
-
-func joinWithSeparators(values []string, separator string, lastSeparator string) string {
-	switch len(values) {
-	case 0:
-		return ""
-	case 1:
-		return values[0]
-	default:
-		sort.Strings(values)
-		return strings.Join(values[0:len(values)-1], separator) + lastSeparator + values[len(values)-1]
-	}
+	return memberNames
 }
