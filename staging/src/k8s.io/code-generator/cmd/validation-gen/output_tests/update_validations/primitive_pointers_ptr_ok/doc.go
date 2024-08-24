40aafe5de34b428057781361f32cf2925a02bf11
Joe Betz <jpbetz@google.com>
2024-08-23 21:42:13 -0400

    Add tests

---
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/update_validations/primitive_pointers_ptr_ok/doc.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +k8s:validation-gen=*
+
+// This is a test package.
+package primitive_pointers_ptr_ok
+
+type T1 struct {
+	// +validateTrue={"flags":["UpdateOnly", "PtrOK"], "msg":"T1.SP, UpdateOnly, PtrOK"}
+	SP *string `json:"sp"`
+	// +validateTrue={"flags":["UpdateOnly", "PtrOK"], "msg":"T1.IP, UpdateOnly, PtrOK"}
+	IP *int `json:"ip"`
+	// +validateTrue={"flags":["UpdateOnly", "PtrOK"], "msg":"T1.BP, UpdateOnly, PtrOK"}
+	BP *bool `json:"bp"`
+	// +validateTrue={"flags":["UpdateOnly", "PtrOK"], "msg":"T1.FP, UpdateOnly, PtrOK"}
+	FP *float64 `json:"fp"`
+}
