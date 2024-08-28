0d96670ecbe6ec67418ec9c8a9309c5c74cdd8dc
Tim Hockin <thockin@google.com>
2024-08-27 17:40:56 -0700

    Make output tests use testscheme

--- staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/update_validations/maps/doc.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/update_validations/maps/doc.go
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 // +k8s:validation-gen=*
+// +k8s:validation-gen-scheme-registry=k8s.io/code-generator/cmd/validation-gen/testscheme.Scheme
 
 // This is a test package.
 package maps
