9ed4253f3d2096c5c069c8624a99c09d25b258e3
Joe Betz <jpbetz@google.com>
2024-09-10 16:06:50 -0400

    Add test utility

--- staging/src/k8s.io/code-generator/cmd/validation-gen/testscheme/testscheme.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/testscheme/testscheme.go
@@ -17,10 +17,22 @@ limitations under the License.
 package testscheme
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/google/go-cmp/cmp"
+	fuzz "github.com/google/gofuzz"
+	"io"
+	"math/rand"
+	"os"
+	"path"
 	"reflect"
+	"sort"
+	"strings"
+	"testing"
 
 	"k8s.io/apimachinery/pkg/api/operation"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -78,4 +90,255 @@ func (s *Scheme) Register(funcs ...func(*Scheme) error) {
 
 func toRegistrationError(err error) *field.Error {
 	return field.InternalError(nil, fmt.Errorf("registration error: %w", err))
+}
+
+// Test returns a ValidationTestBuilder for this scheme.
+func (s *Scheme) Test(t *testing.T) *ValidationTestBuilder {
+	return &ValidationTestBuilder{t, s}
+}
+
+// ValidationTestBuilder provides convenience functions to build
+// validation tests.
+type ValidationTestBuilder struct {
+	*testing.T
+	s *Scheme
+}
+
+const fixtureEnvVar = "UPDATE_VALIDATION_GEN_FIXTURE_DATA"
+
+// ValidateFixtures ensures that the validation errors of all registered types match what is expected by the test fixture files.
+// For each registered type, a value is created for the type, and populated by fuzzing the value, before validating the type.
+// See ValueFuzzed for details.
+//
+// If the UPDATE_VALIDATION_GEN_FIXTURE_DATA=true environment variable is set, test fixture files are created or overridden.
+//
+// Fixtures:
+//   - validate-false.json: defines a map of registered type to a map of field path to  +validateFalse validations args
+//     that are expected to be returned as errors when the type is validated.
+func (s *ValidationTestBuilder) ValidateFixtures() {
+	flag := os.Getenv(fixtureEnvVar)
+	// Run validation
+	got := map[string]map[string][]string{}
+	for t := range s.s.validationFuncs {
+		var v any
+		if t.Kind() == reflect.Ptr {
+			v = reflect.New(t.Elem()).Interface()
+		} else {
+			v = reflect.Indirect(reflect.New(t)).Interface()
+		}
+		if reflect.TypeOf(v).Kind() != reflect.Ptr {
+			v = &v
+		}
+		s.ValueFuzzed(v)
+		vt := &ValidationTester{ValidationTestBuilder: s, value: v}
+		byPath := vt.ValidateFalseArgsByPath()
+		got[t.String()] = byPath
+	}
+
+	testdataFilename := "testdata/validate-false.json"
+	if flag == "true" {
+		// Generate fixture file
+		if err := os.MkdirAll(path.Dir(testdataFilename), os.FileMode(0755)); err != nil {
+			s.Fatal("error making directory", err)
+		}
+		data, err := json.MarshalIndent(got, "  ", "  ")
+		if err != nil {
+			s.Fatal(err)
+		}
+		err = os.WriteFile(testdataFilename, data, os.FileMode(0644))
+		if err != nil {
+			s.Fatal(err)
+		}
+	} else {
+		// Load fixture file
+		testdataFile, err := os.Open(testdataFilename)
+		if errors.Is(err, os.ErrNotExist) {
+			s.Fatalf("%s test fixture data not found. Run go test with the environment variable %s=true to create test fixture data.",
+				testdataFilename, fixtureEnvVar)
+		} else if err != nil {
+			s.Fatal(err)
+		}
+		defer testdataFile.Close()
+
+		byteValue, err := io.ReadAll(testdataFile)
+		testdata := map[string]map[string][]string{}
+		err = json.Unmarshal(byteValue, &testdata)
+		if err != nil {
+			s.Fatal(err)
+		}
+		// Compare fixture with validation results
+		if !cmp.Equal(got, testdata) {
+			s.Errorf("validateFalse args, grouped by field path, differed from %s:\n"+
+				"%s\n"+
+				"If the test expectations have changed, run go test with the environment variable %s=true",
+				testdataFilename, cmp.Diff(got, testdata), fixtureEnvVar)
+		}
+	}
+
+}
+
+func fuzzer() *fuzz.Fuzzer {
+	return fuzz.New().NilChance(0.0).NumElements(1, 1).RandSource(rand.NewSource(0))
+}
+
+// ValueFuzzed automatically populates the given value using a deterministic fuzzer.
+// The fuzzer sets pointers to values and always includes a two map keys and slice elements.
+func (s *ValidationTestBuilder) ValueFuzzed(value any) *ValidationTester {
+	fuzzer().Fuzz(value)
+	return &ValidationTester{ValidationTestBuilder: s, value: value}
+}
+
+// Value returns a ValidationTester for the given value. The value
+// must be a registered with the scheme for validation.
+func (s *ValidationTestBuilder) Value(value any) *ValidationTester {
+	return &ValidationTester{ValidationTestBuilder: s, value: value}
+}
+
+// ValidationTester provides convenience functions to define validation
+// tests for a validatable value.
+type ValidationTester struct {
+	*ValidationTestBuilder
+	value    any
+	oldValue any
+}
+
+// OldValue sets the oldValue for this ValidationTester. When oldValue is set to
+// a non-nil value, update validation will be used to test validation.
+// oldValue must be the same type as value.
+// Returns ValidationTester to support call chaining.
+func (v *ValidationTester) OldValue(oldValue any) *ValidationTester {
+	v.oldValue = oldValue
+	return v
+}
+
+// OldValueFuzzed automatically populates the given value using a deterministic fuzzer.
+// The fuzzer sets pointers to values and always includes a two map keys and slice elements.
+func (v *ValidationTester) OldValueFuzzed(oldValue any) *ValidationTester {
+	fuzzer().Fuzz(oldValue)
+	v.oldValue = oldValue
+	return v
+}
+
+// ExpectValid validates the value and calls t.Errorf if any validation errors are returned.
+// Returns ValidationTester to support call chaining.
+func (v *ValidationTester) ExpectValid() *ValidationTester {
+	v.T.Run(fmt.Sprintf("%T", v.value), func(t *testing.T) {
+		errs := v.validate()
+		if len(errs) > 0 {
+			t.Errorf("want no errors, got: %v", errs)
+		}
+	})
+	return v
+}
+
+// ExpectValidAt validates the value and calls t.Errorf for any validation errors at the given path.
+// Returns ValidationTester to support call chaining.
+func (v *ValidationTester) ExpectValidAt(fldPath *field.Path) *ValidationTester {
+	v.T.Run(fmt.Sprintf("%T.%v", v.value, fldPath), func(t *testing.T) {
+		var got field.ErrorList
+		for _, e := range v.validate() {
+			if e.Field == fldPath.String() {
+				got = append(got, e)
+			}
+		}
+		if len(got) > 0 {
+			t.Errorf("want no errors at %v, got: %v", fldPath, got)
+		}
+	})
+	return v
+}
+
+// ExpectInvalid validates the value and calls t.Errorf if want does not match the actual errors.
+// Returns ValidationTester to support call chaining.
+func (v *ValidationTester) ExpectInvalid(want ...*field.Error) *ValidationTester {
+	return v.expectInvalid(byFullError, want...)
+}
+
+// ExpectValidateFalse validates the value and calls t.Errorf if the actual errors do not
+// match the given validateFalseArgs.  For example, if the value to validate has a
+// single `+validateFalse="type T1"` tag, ExpectValidateFalse("type T1") will pass.
+// Returns ValidationTester to support call chaining.
+func (v *ValidationTester) ExpectValidateFalse(validateFalseArgs ...string) *ValidationTester {
+	var want []*field.Error
+	for _, s := range validateFalseArgs {
+		want = append(want, field.Invalid(nil, "", fmt.Sprintf("forced failure: %s", s)))
+	}
+	return v.expectInvalid(byDetail, want...)
+}
+
+func (v *ValidationTester) ExpectValidateFalseByPath(validateFalseArgsByField map[string][]string) *ValidationTester {
+	v.T.Run(fmt.Sprintf("%T", v.value), func(t *testing.T) {
+		byField := v.ValidateFalseArgsByPath()
+		// ensure args are sorted
+		for _, args := range validateFalseArgsByField {
+			sort.Strings(args)
+		}
+		if !cmp.Equal(validateFalseArgsByField, byField) {
+			t.Errorf("validateFalse args, grouped by field path, differed from expected:\n%s\n", cmp.Diff(validateFalseArgsByField, byField))
+		}
+
+	})
+	return v
+}
+
+func (v *ValidationTester) ValidateFalseArgsByPath() map[string][]string {
+	byField := map[string][]string{}
+	errs := v.validate()
+	for _, e := range errs {
+		if strings.HasPrefix(e.Detail, "forced failure: ") {
+			arg := strings.TrimPrefix(e.Detail, "forced failure: ")
+			f := e.Field
+			if f == "<nil>" {
+				f = ""
+			}
+			byField[f] = append(byField[f], arg)
+		}
+	}
+	// ensure args are sorted
+	for _, args := range byField {
+		sort.Strings(args)
+	}
+	return byField
+}
+
+func (v *ValidationTester) expectInvalid(matcher matcher, errs ...*field.Error) *ValidationTester {
+	v.T.Run(fmt.Sprintf("%T", v.value), func(t *testing.T) {
+		want := sets.New[string]()
+		for _, e := range errs {
+			want.Insert(matcher(e))
+		}
+
+		got := sets.New[string]()
+		for _, e := range v.validate() {
+			got.Insert(matcher(e))
+		}
+		if !got.Equal(want) {
+			t.Errorf("validation errors differed from expected:\n%v\n", cmp.Diff(want, got))
+
+			for x := range got.Difference(want) {
+				fmt.Printf("%q,\n", strings.TrimPrefix(x, "forced failure: "))
+			}
+		}
+	})
+	return v
+}
+
+type matcher func(err *field.Error) string
+
+func byDetail(err *field.Error) string {
+	return err.Detail
+}
+
+func byFullError(err *field.Error) string {
+	return err.Error()
+}
+
+func (v *ValidationTester) validate() field.ErrorList {
+	var errs field.ErrorList
+	if v.oldValue == nil {
+		errs = v.s.Validate(v.value)
+	} else {
+		errs = v.s.ValidateUpdate(v.value, v.oldValue)
+	}
+	return errs
 }
