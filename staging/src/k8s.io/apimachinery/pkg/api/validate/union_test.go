12f5a7b7e8601f656f61f01549e0e023266c26f8
yongruilin <yongrlin@google.com>
2024-11-13 11:21:29 -0800

    fix: update Union validation unit test

--- staging/src/k8s.io/apimachinery/pkg/api/validate/union_test.go
+++ staging/src/k8s.io/apimachinery/pkg/api/validate/union_test.go
@@ -20,6 +20,7 @@ import (
 	"reflect"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/api/operation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
@@ -29,62 +30,57 @@ type testMember struct{}
 func TestUnion(t *testing.T) {
 	testCases := []struct {
 		name        string
-		fields      []any
+		fields      [][2]string
 		fieldValues []any
 		expected    field.ErrorList
 	}{
 		{
 			name:        "valid pointers one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{nil, nil, nil, &testMember{}},
 			expected:    nil,
 		},
 		{
 			name:        "invalid pointers one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{nil, &testMember{}, nil, &testMember{}},
-			expected:    field.ErrorList{field.Invalid(nil, "", "must not specify \"b\" and \"d\", must specify one of: \"a\", \"b\", \"c\" or \"d\"")},
+			expected:    field.ErrorList{field.Invalid(nil, "{b, d}", "must specify exactly one of: `a`, `b`, `c`, `d`")},
 		},
 		{
 			name:        "valid string one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{"", "", "", "x"},
 			expected:    nil,
 		},
 		{
 			name:        "invalid string one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{"", "x", "", "x"},
-			expected:    field.ErrorList{field.Invalid(nil, "", "must not specify \"b\" and \"d\", must specify one of: \"a\", \"b\", \"c\" or \"d\"")},
+			expected:    field.ErrorList{field.Invalid(nil, "{b, d}", "must specify exactly one of: `a`, `b`, `c`, `d`")},
 		},
 		{
 			name:        "valid mixed type one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{0, "", nil, "x"},
 			expected:    nil,
 		},
 		{
 			name:        "invalid mixed type one of",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{0, "x", nil, &testMember{}},
-			expected:    field.ErrorList{field.Invalid(nil, "", "must not specify \"b\" and \"d\", must specify one of: \"a\", \"b\", \"c\" or \"d\"")},
+			expected:    field.ErrorList{field.Invalid(nil, "{b, d}", "must specify exactly one of: `a`, `b`, `c`, `d`")},
 		},
 		{
 			name:        "invalid no member set",
-			fields:      []any{"a", "b", "c", "d"},
+			fields:      [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			fieldValues: []any{nil, nil, nil, nil},
-			expected:    field.ErrorList{field.Invalid(nil, "", "must specify one of: \"a\", \"b\", \"c\" or \"d\"")},
-		},
-		{
-			name:        "valid multiple field for union member",
-			fields:      []any{[2]string{"a", "x"}, "b", "c", [2]string{"d", "x"}},
-			fieldValues: []any{1, nil, nil, 1},
+			expected:    field.ErrorList{field.Invalid(nil, "", "must specify exactly one of: `a`, `b`, `c`, `d`")},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := Union(nil, &testUnion{}, NewUnionMembership(tc.fields...), tc.fieldValues...)
+			got := Union(operation.Context{Operation: operation.Update}, nil, nil, nil, NewUnionMembership(tc.fields...), tc.fieldValues...)
 			if !reflect.DeepEqual(got, tc.expected) {
 				t.Errorf("got %v want %v", got, tc.expected)
 			}
@@ -96,7 +92,7 @@ func TestDiscriminatedUnion(t *testing.T) {
 	testCases := []struct {
 		name               string
 		discriminatorField string
-		fields             []any
+		fields             [][2]string
 		discriminatorValue string
 		fieldValues        []any
 		expected           field.ErrorList
@@ -104,33 +100,26 @@ func TestDiscriminatedUnion(t *testing.T) {
 		{
 			name:               "valid discriminated union",
 			discriminatorField: "d",
-			fields:             []any{[2]string{"a", "A"}, "b", "c", [2]string{"d", "D"}},
+			fields:             [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			discriminatorValue: "A",
 			fieldValues:        []any{1, nil, nil, nil},
 		},
 		{
-			name:               "valid discriminated union, implicit union name",
-			discriminatorField: "type",
-			fields:             []any{[2]string{"a", "A"}, "b", "c", [2]string{"d", "D"}},
-			discriminatorValue: "B",
-			fieldValues:        []any{nil, 1, nil, nil},
-		},
-		{
 			name:               "invalid, discriminator not set to member that is specified",
 			discriminatorField: "type",
-			fields:             []any{[2]string{"a", "A"}, "b", "c", [2]string{"d", "D"}},
+			fields:             [][2]string{{"a", "A"}, {"b", "B"}, {"c", "C"}, {"d", "D"}},
 			discriminatorValue: "C",
 			fieldValues:        []any{nil, 1, nil, nil},
 			expected: field.ErrorList{
-				field.Invalid(field.NewPath("b"), "", "must not be specified when \"type\" is \"C\""),
-				field.Invalid(field.NewPath("c"), "", "must be specified when \"type\" is \"C\""),
+				field.Invalid(field.NewPath("b"), "", "may only be specified when `type` is \"B\""),
+				field.Invalid(field.NewPath("c"), "", "must be specified when `type` is \"C\""),
 			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := DiscriminatedUnion(nil, &testUnion{}, NewDiscriminatedUnionMembership(tc.discriminatorField, tc.fields...), tc.discriminatorValue, tc.fieldValues...)
+			got := DiscriminatedUnion(operation.Context{Operation: operation.Update}, nil, nil, nil, NewDiscriminatedUnionMembership(tc.discriminatorField, tc.fields...), tc.discriminatorValue, tc.fieldValues...)
 			if !reflect.DeepEqual(got, tc.expected) {
 				t.Errorf("got %v want %v", got.ToAggregate(), tc.expected.ToAggregate())
 			}
