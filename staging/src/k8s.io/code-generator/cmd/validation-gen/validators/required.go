kubernetes/code-generator

Handle optional value-types with defaults
Kubernetes-commit: 92aeb63a5be90bdcc044aa820d5cc014b4cbb65b

cmd/validation-gen/validators/required.go
@@ -17,16 +17,21 @@ limitations under the License.
 package validators
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
+
+	"k8s.io/gengo/v2"
+	"k8s.io/gengo/v2/types"
 
 	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/gengo/v2/types"
 )
 
 const (
 	requiredTagName  = "k8s:required"
 	optionalTagName  = "k8s:optional"
 	forbiddenTagName = "k8s:forbidden"
+	defaultTagName   = "default" // TODO: this should evenually be +k8s:default
 )
 
 func init() {
@@ -84,7 +89,7 @@ var (
 
 // TODO: It might be valuable to have a string payload for when requiredness is
 // conditional (e.g. required when <otherfield> is specified).
-func (requirednessTagValidator) doRequired(context Context) (Validations, error) {
+func (rtv requirednessTagValidator) doRequired(context Context) (Validations, error) {
 	// Most validators don't care whether the value they are validating was
 	// originally defined as a value-type or a pointer-type in the API.  This
 	// one does.  Since Go doesn't do partial specialization of templates, we
@@ -114,7 +119,48 @@ var (
 	optionalMapValidator     = types.Name{Package: libValidationPkg, Name: "OptionalMap"}
 )
 
-func (requirednessTagValidator) doOptional(context Context) (Validations, error) {
+func (rtv requirednessTagValidator) doOptional(context Context) (Validations, error) {
+	// All of our tags are expressed from the perspective of a client of the
+	// API, but the code we generate is for the server. Optional is tricky.
+	//
+	// A field which is marked as optional and does not have a default is
+	// strictly optional. A client is allowed to not set it and the server will
+	// not give it a default value. Code which consumes it must handle that it
+	// might not have any value at all.
+	//
+	// A field which is marked as optional but has a default is optional to
+	// clients, but required to the server. A client is allowed to not set it
+	// but the server will give it a default value. Code which consumes it can
+	// assume that it always has a value.
+	//
+	// One special case must be handled: optional non-pointer fields with
+	// default values. If the default is not the zero value for the type, then
+	// the zero value is used to decide whether to assign the default value,
+	// and so must be out of bounds; we can proceed as above.
+	//
+	// But if the default is the zero value, then the zero value is obviously
+	// valid, and the fact that the field is optional is meaningless - there is
+	// no way to tell the difference between a client not setting it (yielding
+	// the zero value) and a client setting it to the zero value.
+	//
+	// TODO: handle default=ref(...)
+	// TODO: handle manual defaulting
+	if hasDefault, zeroDefault, err := rtv.hasZeroDefault(context); err != nil {
+		return Validations{}, err
+	} else if hasDefault {
+		if !isNilableType(context.Type) && zeroDefault {
+			return Validations{Comments: []string{"optional value-type fields with zero-value defaults are purely documentation"}}, nil
+		}
+		validations, err := rtv.doRequired(context)
+		if err != nil {
+			return Validations{}, err
+		}
+		for i, fn := range validations.Functions {
+			validations.Functions[i] = WithComment(fn, "optional fields with default values are effectively required")
+		}
+		return validations, nil
+	}
+
 	// Most validators don't care whether the value they are validating was
 	// originally defined as a value-type or a pointer-type in the API.  This
 	// one does.  Since Go doesn't do partial specialization of templates, we
@@ -135,6 +181,71 @@ func (requirednessTagValidator) doOptional(context Context) (Validations, error)
 		return Validations{}, fmt.Errorf("non-pointer structs cannot use the %q tag", optionalTagName)
 	}
 	return Validations{Functions: []FunctionGen{Function(optionalTagName, ShortCircuit|NonError, optionalValueValidator)}}, nil
+}
+
+// hasZeroDefault returns whether the field has a default value and whether
+// that default value is the zero value for the field's type.
+func (rtv requirednessTagValidator) hasZeroDefault(context Context) (bool, bool, error) {
+	t := realType(context.Type)
+	// This validator only applies to fields, so Member must be valid.
+	tagsByName, err := gengo.ExtractFunctionStyleCommentTags("+", []string{defaultTagName}, context.Member.CommentLines)
+	if err != nil {
+		return false, false, fmt.Errorf("failed to read tags: %w", err)
+	}
+
+	tags, hasDefault := tagsByName[defaultTagName]
+	if !hasDefault {
+		return false, false, nil
+	}
+	if len(tags) == 0 {
+		return false, false, fmt.Errorf("+default tag with no value")
+	}
+	if len(tags) > 1 {
+		return false, false, fmt.Errorf("+default tag with multiple values: %q", tags)
+	}
+
+	payload := tags[0].Value
+	var defaultValue any
+	if err := json.Unmarshal([]byte(payload), &defaultValue); err != nil {
+		return false, false, fmt.Errorf("failed to parse default value %q: %w", payload, err)
+	}
+	if defaultValue == nil {
+		return false, false, fmt.Errorf("failed to parse default value %q: unmarshalled to nil", payload)
+	}
+
+	zero, found := typeZeroValue[t.String()]
+	if !found {
+		return false, false, fmt.Errorf("unknown zero-value for type %s", t.String())
+	}
+
+	return true, reflect.DeepEqual(defaultValue, zero), nil
+}
+
+// This is copied from defaulter-gen.
+// TODO: move this back to gengo as Type.ZeroValue()?
+var typeZeroValue = map[string]any{
+	"uint":        0.,
+	"uint8":       0.,
+	"uint16":      0.,
+	"uint32":      0.,
+	"uint64":      0.,
+	"int":         0.,
+	"int8":        0.,
+	"int16":       0.,
+	"int32":       0.,
+	"int64":       0.,
+	"byte":        0.,
+	"float64":     0.,
+	"float32":     0.,
+	"bool":        false,
+	"time.Time":   "",
+	"string":      "",
+	"integer":     0.,
+	"number":      0.,
+	"boolean":     false,
+	"[]byte":      "", // base64 encoded characters
+	"interface{}": interface{}(nil),
+	"any":         interface{}(nil),
 }
 
 var (

yongruilin/kubernetes

staging/src/k8s.io/code-generator/cmd/validation-gen/validators/required.go
@@ -17,7 +17,9 @@ limitations under the License.
 package validators
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/gengo/v2"
@@ -25,20 +27,8 @@ import (
 )
 
 const (
-	// All of our tags are expressed from the perspective of a client of the
-	// API, but the code we generate is for the server. Optional is tricky.
-	//
-	// A field which is marked as optional and does not have a default is
-	// strictly optional. A client is allowed to not set it and the server will
-	// not give it a default value. Code which consumes it must handle that it
-	// might not have any value at all.
-	//
-	// A field which is marked as optional but has a default is optional to
-	// clients, but required to the server. A client is allowed to not set it
-	// but the server will give it a default value. Code which consumes it can
-	// assume that it always has a value.
-	optionalTagName  = "k8s:optional"
 	requiredTagName  = "k8s:required"
+	optionalTagName  = "k8s:optional"
 	forbiddenTagName = "k8s:forbidden"
 	defaultTagName   = "default" // TODO: this should evenually be +k8s:default
 )
@@ -98,7 +88,7 @@ var (
 
 // TODO: It might be valuable to have a string payload for when requiredness is
 // conditional (e.g. required when <otherfield> is specified).
-func (requirednessTagValidator) doRequired(context Context) (Validations, error) {
+func (rtv requirednessTagValidator) doRequired(context Context) (Validations, error) {
 	// Most validators don't care whether the value they are validating was
 	// originally defined as a value-type or a pointer-type in the API.  This
 	// one does.  Since Go doesn't do partial specialization of templates, we
@@ -127,12 +117,45 @@ var (
 )
 
 func (rtv requirednessTagValidator) doOptional(context Context) (Validations, error) {
-	// Peek at the default tag to determine if this field is actually required.
-	// See comments on optionalTagName.
+	// All of our tags are expressed from the perspective of a client of the
+	// API, but the code we generate is for the server. Optional is tricky.
 	//
-	// This tag only applies to fields, so Member must be valid.
-	if _, hasDefault := gengo.ExtractCommentTags("+", context.Member.CommentLines)[defaultTagName]; hasDefault {
-		return rtv.doRequired(context)
+	// A field which is marked as optional and does not have a default is
+	// strictly optional. A client is allowed to not set it and the server will
+	// not give it a default value. Code which consumes it must handle that it
+	// might not have any value at all.
+	//
+	// A field which is marked as optional but has a default is optional to
+	// clients, but required to the server. A client is allowed to not set it
+	// but the server will give it a default value. Code which consumes it can
+	// assume that it always has a value.
+	//
+	// One special case must be handled: optional non-pointer fields with
+	// default values. If the default is not the zero value for the type, then
+	// the zero value is used to decide whether to assign the default value,
+	// and so must be out of bounds; we can proceed as above.
+	//
+	// But if the default is the zero value, then the zero value is obviously
+	// valid, and the fact that the field is optional is meaningless - there is
+	// no way to tell the difference between a client not setting it (yielding
+	// the zero value) and a client setting it to the zero value.
+	//
+	// TODO: handle default=ref(...)
+	// TODO: handle manual defaulting
+	if hasDefault, zeroDefault, err := rtv.hasZeroDefault(context); err != nil {
+		return Validations{}, err
+	} else if hasDefault {
+		if !isNilableType(context.Type) && zeroDefault {
+			return Validations{Comments: []string{"optional value-type fields with zero-value defaults are purely documentation"}}, nil
+		}
+		validations, err := rtv.doRequired(context)
+		if err != nil {
+			return Validations{}, err
+		}
+		if len(validations.Functions) > 0 {
+			validations.Functions[0] = WithComment(validations.Functions[0], "optional fields with default values are effectively required")
+		}
+		return validations, nil
 	}
 
 	// Most validators don't care whether the value they are validating was
@@ -154,6 +177,71 @@ func (rtv requirednessTagValidator) doOptional(context Context) (Validations, er
 		return Validations{Comments: []string{"optional non-pointer structs are purely documentation"}}, nil
 	}
 	return Validations{Functions: []FunctionGen{Function(optionalTagName, ShortCircuit|NonError, optionalValueValidator)}}, nil
+}
+
+// hasZeroDefault returns whether the field has a default value and whether
+// that default value is the zero value for the field's type.
+func (rtv requirednessTagValidator) hasZeroDefault(context Context) (bool, bool, error) {
+	t := realType(context.Type)
+	// This validator only applies to fields, so Member must be valid.
+	tagsByName, err := gengo.ExtractFunctionStyleCommentTags("+", []string{defaultTagName}, context.Member.CommentLines)
+	if err != nil {
+		return false, false, fmt.Errorf("failed to read tags: %v", err)
+	}
+
+	tags, hasDefault := tagsByName[defaultTagName]
+	if !hasDefault {
+		return false, false, nil
+	}
+	if len(tags) == 0 {
+		return false, false, fmt.Errorf("+default tag with no value")
+	}
+	if len(tags) > 1 {
+		return false, false, fmt.Errorf("+default tag with multiple values: %q", tags)
+	}
+
+	payload := tags[0].Value
+	var defaultValue any
+	if err := json.Unmarshal([]byte(payload), &defaultValue); err != nil {
+		return false, false, fmt.Errorf("failed to parse default value %q: %v", payload, err)
+	}
+	if defaultValue == nil {
+		return false, false, fmt.Errorf("failed to parse default value %q: unmarshalled to nil", payload)
+	}
+
+	zero, found := typeZeroValue[t.String()]
+	if !found {
+		return false, false, fmt.Errorf("unknown zero-value for type %s", t.String())
+	}
+
+	return true, reflect.DeepEqual(defaultValue, zero), nil
+}
+
+// This is copied from defaulter-gen.
+// TODO: move this back to gengo as Type.ZeroValue()?
+var typeZeroValue = map[string]any{
+	"uint":        0.,
+	"uint8":       0.,
+	"uint16":      0.,
+	"uint32":      0.,
+	"uint64":      0.,
+	"int":         0.,
+	"int8":        0.,
+	"int16":       0.,
+	"int32":       0.,
+	"int64":       0.,
+	"byte":        0.,
+	"float64":     0.,
+	"float32":     0.,
+	"bool":        false,
+	"time.Time":   "",
+	"string":      "",
+	"integer":     0.,
+	"number":      0.,
+	"boolean":     false,
+	"[]byte":      "", // base64 encoded characters
+	"interface{}": interface{}(nil),
+	"any":         interface{}(nil),
 }
 
 var (