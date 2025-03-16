a1d2d5c585e5dc5e0b6bcdcefd103e4e8184d15a
Tim Hockin <thockin@google.com>
2025-03-16 14:02:55 -0700

    Revert small diffs vs. master
    This reverts commit 178b781a6d6cc8a3324aeb9c9e177d86a62c9b65.

--- pkg/features/versioned_kube_features.go
+++ pkg/features/versioned_kube_features.go
@@ -549,7 +549,8 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	OrderedNamespaceDeletion: {
-		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.30"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	PersistentVolumeLastPhaseTransitionTime: {
@@ -627,6 +628,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	RecursiveReadOnlyMounts: {
 		{Version: version.MustParse("1.30"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("1.31"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("1.33"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.36
 	},
 
 	ReduceDefaultCrashLoopBackOffDecay: {
