de6fdd8db4f3e07641692790d1e7e8c780c8c476
Tim Hockin <thockin@google.com>
2025-05-23 17:24:41 -0700

    Move utils to a pkg, add tests

--- pkg/apis/core/validation/validation_test.go
+++ pkg/apis/core/validation/validation_test.go
@@ -22540,7 +22540,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
 			},
 		},
 		"invalid legacy IP with strict validation": {
@@ -22552,7 +22552,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
 			},
 		},
 		"Multiple ports, one without name": {
@@ -22600,7 +22600,7 @@ func TestValidateEndpointsCreate(t *testing.T) {
 				}},
 			},
 			expectedErrs: field.ErrorList{
-				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip"),
+				field.Invalid(field.NewPath("subsets[0].addresses[0].ip"), nil, "").WithOrigin("format=k8s-ip-sloppy"),
 			},
 		},
 		"Port missing number": {
